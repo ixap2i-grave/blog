02601b77d8c5e505fe264edea8d79c9f3a3c4189
ixap2i <ayamashita124@gmail.com>
2018-10-26 01:57:38 +0900

    add article model and migrations

--- db/migrate/20181025163117_create_articles.rb
+++ db/migrate/20181025163117_create_articles.rb
@@ -1,7 +1,9 @@
-class CreateArticles < ActiveRecord::Migration[5.2]
+class Article < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
+      t.string :name
+      t.string :tag_id
+      t.text :body
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_163117) do
+ActiveRecord::Schema.define(version: 2018_10_25_164837) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "namespace"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2018_10_25_163117) do
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "tag_id"
+    t.text "body"
   end
 
 end
