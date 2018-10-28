73bb15677bc186f3e9024ddb9729398e69986b5a
ixap2i <ayamashita124@gmail.com>
2018-10-28 15:15:20 +0900

    add uploaders

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,6 +1,7 @@
 class Ckeditor::Asset < ActiveRecord::Base
+  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
-
+  include Ckeditor::Backend::Paperclip
   delegate :url, :current_path, :content_type, to: :data
 
   validates :data, presence: true
