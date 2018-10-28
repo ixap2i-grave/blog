ea153f90c57e929b05ed7c40e4865a113f263694
ixap2i <ayamashita124@gmail.com>
2018-10-28 14:23:31 +0900

    disable mecab to heroku

--- app/models/concerns/Mecab.rb
+++ app/models/concerns/Mecab.rb
@@ -1,8 +1,8 @@
-module Mecab
-  require 'mecab'
+# module Mecab
+#   require 'mecab'
 
-  def search_associate_words terms
-    paragraph =  MeCab::Tagger.new
-    paragraph.parse terms
-  end
-end
+#   def search_associate_words terms
+#     paragraph =  MeCab::Tagger.new
+#     paragraph.parse terms
+#   end
+# end
